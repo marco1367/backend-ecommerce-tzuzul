a701a2f001c95f72308cced70225e8fcacf4ee03
marco <marcopablodemaio@gmail.com>
2022-04-12 15:09:39 -0300

    start script change in packege.json

--- index.js
+++ index.js
@@ -11,138 +11,138 @@ conn.sync({ force: true })
     .then(async () => {
         console.log('sincroniza');
 
-        const all_products = [
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 2',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-            {
-                name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
-                image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
-                price: 240,
-            },
-        ]
+        // const all_products = [
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 2',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        //     {
+        //         name: 'producto 1 qsdasdasdasd asdasdasdasd adgdfgsdfg sdghsrgjheryj',
+        //         image: 'https://http2.mlstatic.com/D_NQ_NP_983745-MLA41071258804_032020-O.webp',
+        //         price: 240,
+        //     },
+        // ]
 
-        Products.bulkCreate(all_products).then(console.log('Carga de datos de PRODUCTOS correcta'));
-        Rolle.bulkCreate(rolles).then(console.log('Carga de datos ROLLES correcta'));
+        // Products.bulkCreate(all_products).then(console.log('Carga de datos de PRODUCTOS correcta'));
+        // Rolle.bulkCreate(rolles).then(console.log('Carga de datos ROLLES correcta'));
     
-        server.listen(process.env.PORT || 5000, () => {
+        server.listen(process.env.PORT, () => {
             console.log(`the server has listen port ${PORT}`);
         });
     })
