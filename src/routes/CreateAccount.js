076067c4bec1e37967a6f27433cffc08ad14a970
marco <marcopablodemaio@gmail.com>
2022-04-13 15:11:35 -0300

    comentamos el meadleware multer en el controller createAccount

--- src/routes/CreateAccount.js
+++ src/routes/CreateAccount.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const uplead = require('../meaddlewares/multerFIles')
 const {PostCreateAccount} = require('../controllers/PostCreateAccount');
 const multer  = require('multer');
-const upload = multer({ dest: '../images/uploads' });
+// const upload = multer({ dest: '../images/uploads' });
 
 
 const router = Router();
